6ec30e5b49eff05954d71d87a0772d17af7d44e8
yang.zhenzhen <soul11201@gmail.com>
2016-11-15 14:04:03 +0800

    update hosts bat

---
+++ hosts.bat
@@ -0,0 +1,4 @@
+wget  --no-check-certificate  https://raw.githubusercontent.com/racaljk/hosts/master/hosts -O %SYSTEMROOT%/System32/drivers/etc/hosts
+pause
+
+rem -O %SYSTEMROOT%/System32/drivers/etc/hosts
